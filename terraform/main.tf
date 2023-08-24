1710f746408d54e40a1566fc9e820b4a87dae3bb
Poss111 <rixxroid@gmail.com>
2023-08-23 23:38:39 -0500

    Attempts are over 9000

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,14 @@ provider "aws" {
   }
 }
 
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
-    command     = "aws"
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
-  }
+  token                  = data.aws_eks_cluster_auth.default.token
 }
 
 data "aws_caller_identity" "current" {}
