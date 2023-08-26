fb1f88f4e6811a54f28cff90b9ec77ea1f8c1fa6
Poss111 <rixxroid@gmail.com>
2023-08-25 20:02:35 -0500

    Adding back aws_auth_roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,15 @@ provider "aws" {
   }
 }
 
-data "aws_eks_cluster_auth" "default" {
-  name       = module.eks.cluster_id
-  depends_on = [ module.eks ]
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.default.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id,  "--region", var.region]
+  }
 }
 
 data "aws_caller_identity" "current" {}
