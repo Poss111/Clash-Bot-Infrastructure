fc5ecc5fa461d77e18e5a90caab75d237fdfe984
Daniel Poss <rixxroid@gmail.com>
2025-02-17 11:48:43 -0600

    Change EKS node group instance type from t3.small to t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.micro"]
 
       min_size     = 1
       max_size     = 2
