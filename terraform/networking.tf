46b1dd8bfbfddcfa0fb7e99096c669bed7846054
Daniel Poss <rixxroid@gmail.com>
2025-03-02 13:00:14 -0600

    Add S3 VPC endpoint configuration with Gateway type

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -130,3 +130,10 @@ resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_endpoint_type = "Interface"
   subnet_ids        = [aws_subnet.private_1.id, aws_subnet.private_2.id]
 }
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+}
