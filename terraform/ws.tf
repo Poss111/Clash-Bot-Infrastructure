fdf5d317fd4a63e31594654b143be9df629ca00f
Daniel Poss <rixxroid@gmail.com>
2025-03-27 20:16:12 -0500

    Rename API Gateway resources for WebSocket integration and enhance clarity

--- terraform/ws.tf
+++ terraform/ws.tf
@@ -1,6 +1,6 @@
 
 # Step 5: Create the API Gateway V2 custom domain
-resource "aws_apigatewayv2_domain_name" "custom_domain" {
+resource "aws_apigatewayv2_domain_name" "ws_custom_domain" {
   domain_name = "ws.clash-bot.ninja"
 
   domain_name_configuration {
@@ -10,7 +10,7 @@ resource "aws_apigatewayv2_domain_name" "custom_domain" {
   }
 }
 
-resource "aws_apigatewayv2_api" "api" {
+resource "aws_apigatewayv2_api" "ws_api" {
   name                         = "ClashBot_Services_WS"
   protocol_type                = "WEBSOCKET"
   description                  = "High-performance API Gateway for ClashBot Services, enabling seamless integration and robust communication."
@@ -24,21 +24,21 @@ resource "aws_apigatewayv2_api" "api" {
 }
 
 # Create a default route ($default)
-resource "aws_apigatewayv2_route" "default_route" {
+resource "aws_apigatewayv2_route" "ws_default_route" {
   api_id    = aws_apigatewayv2_api.websocket_api.id
   route_key = "$default"
   target    = "integrations/TO_BE_DEFINED_IN_PULUMI"
 }
 
 # Step 7: Map the custom domain to API Gateway
-resource "aws_apigatewayv2_api_mapping" "api_mapping" {
+resource "aws_apigatewayv2_api_mapping" "ws_api_mapping" {
   api_id      = aws_apigatewayv2_api.api.id
   domain_name = aws_apigatewayv2_domain_name.custom_domain.id
   stage       = "$default"
 }
 
 # Step 8: Create a Route 53 Alias Record for API Gateway
-resource "aws_route53_record" "api_dns" {
+resource "aws_route53_record" "ws_dns" {
   zone_id = data.aws_route53_zone.existing_zone.zone_id
   name    = "ws.clash-bot.ninja"
   type    = "A"
